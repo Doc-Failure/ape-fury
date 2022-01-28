b76a1fe65ca4b8a6e73a0c8ea72fe5424936f4eb
Silver <stefano.convertino@gmail.com>
2022-01-28 17:23:14 +0100

    ERC20Factory Updated

--- contracts/ERC20TokenFactory.sol
+++ contracts/ERC20TokenFactory.sol
@@ -5,21 +5,25 @@ import "./ERC20Token.sol";
 contract ERC20TokenFactory {
 
   // index of created contracts
+  mapping(address => address) public lastDeployedContract;
+
   address[] public contracts;
 
   function getContracts() public view returns(address[] memory){
     return contracts;
   }
 
-  // deploy a new contract
-  function deployNewToken(string memory name, string memory symbol, uint initialSupply) 
-    public returns(address newContract){
-      ERC20Token token = new ERC20Token(name,symbol,initialSupply);
-      address contractAddress = address(token);
-      contracts.push(contractAddress);
+  function getLastDeployedContract() public view returns(address){
+    return lastDeployedContract[msg.sender];
+  }
 
-      token.transfer(msg.sender, token.totalSupply());
-      
-      return contractAddress;
-    }
+  // deploy a new contract
+  function deployNewToken(string memory name, string memory symbol, uint initialSupply) public{
+    ERC20Token token = new ERC20Token(name,symbol,initialSupply);
+    address contractAddress = address(token);
+    contracts.push(contractAddress);
+    lastDeployedContract[msg.sender]=contractAddress;
+
+    token.transfer(msg.sender, token.totalSupply());
+  }
 }
